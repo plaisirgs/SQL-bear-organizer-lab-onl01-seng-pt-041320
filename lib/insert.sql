b7f77df223223d2ee51c47f14accb3c4e80b2f55
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-06-20 18:22:44 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES 
+(1, "Mr. Chocolate", 3, "male","black", "aggressive", 1),
+(2, "Rowdy", 4, "female", "blue", "gentle", 1),
+(3, "Tabitha", 2, "male", "purple", "quiet", 1),
+(4, "Sergeant Brown", 1, "male", "orange", "gentle", 1),
+(5, "Melissa", 5, "male", "yellow", "nice", 1),
+(6, "Grinch", 7, "male", "white", "gentle", 1),
+(7, "Wendy", 6, "female", "green", "aggressive", 1),
+(8, NULL, 8, "male", "gold", "nice", 0);
+
+
+
+
+
+
+
+
+
+
+
